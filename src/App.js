5f22844cbdf939948120d2355aa1463b408e7881
SKINNYLEGS10 <tennison.schmidt@svu.edu>
2022-09-21 13:47:43 -0400

    added button to page.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Typography} from '@mui/material'
+import SaveIcon from '@mui/icons-material/Save';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <>
+    <Layout></Layout>
+    </>
+  );
+}
+
+export default App;
